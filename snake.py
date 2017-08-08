c6b1476675cd3534972f682df6b9dca9e301401f
jerrita <782981223@qq.com>
2017-08-08 20:02:26 +0800

    新增 随机生成食物 生成蛇

--- snake.py
+++ snake.py
@@ -1,10 +1,10 @@
 import snwin
 
-win = snwin.snwin(20,40)
+win = snwin.snwin(20,20)
 win.draw()
 
 while True:
 	di = input('')
 	if di == '2' or di == '4' or di == '5' or di == '6':
-		win.snake.go(di)
+		win.go(di)
 		win.draw()
--- snwin.py
+++ snwin.py
@@ -1,22 +1,48 @@
 import os
+import random
 
 class snwin:
 	def __init__(self,wid,hei):
 		self.width = wid
 		self.height = hei
 		self.snmap = []
+		self.sndire = '6'
+		self.snbody = [(2,2),(3,2)]
+		self.snhead = (3,2)
+		self.sntail = (2,2)
+		self.snshow = '*'
 		
 		#地图初始化
 		for i in range(0,hei):
 			self.snmap.append([])
 			for j in range(0,wid):
-				self.snmap[i].append('  ')
+				self.snmap[i].append(' ')
 				if i == 0 or i == hei - 1 or j == 0 or j == wid - 1:
-					self.snmap[i][j] = '# '
+					self.snmap[i][j] = '#'
+					
+		self.spanfood()
+					
 
 	def draw(self):
 		os.system('clear')
+		self.addsnake()
 		for i in self.snmap:
 			for j in i:
-				print(j,end='')
+				print(j,end=' ')
 			print('')
+			
+	def mpadd(self,x,y,z):
+		self.snmap[y][x] = z
+	def mpdel(self,x,y):
+		self.snmap[y][x] = ' '
+	def addsnake(self):
+		for i in self.snbody:
+			self.mpadd(i[0],i[1],self.snshow)
+	def spanfood(self):
+		x = random.randint(1,self.width - 1)
+		y = random.randint(1,self.height - 1)
+		if (x,y) in self.snbody:
+			self.sneat()
+		else:
+			self.food = (x,y)
+			self.mpadd(x,y,'@')
