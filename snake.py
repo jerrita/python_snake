22b51ff3852d3d22d7283d042a41f539cb34135c
jerrita <782981223@qq.com>
2017-08-09 12:30:39 +0800

    傻逼式ai完成，第一版结束

--- snake.py
+++ snake.py
@@ -5,13 +5,26 @@ import os
 win = snwin.snwin(15,15)
 win.draw()
 
+def ai(a,b):
+	if a[1] < b[1]:
+		return '5'
+	if a[1] > b[1]:
+		return '2'
+	if a[1] == b[1]:
+		if a[0] < b[0]:
+			return '6'
+		if a[0] > b[0]:
+			return '4'
+
 while True:
-	win.go(input(''))
+	next = ai(win.snhead,win.food)
+	win.go(next)
 	if win.lose == True:
 		break
 	win.clearmap()
 	win.draw()
-	time.sleep(0.5)
+	time.sleep(0.05)
 
 os.system('clear')
 print('游戏结束！得分:',win.scor)
+
